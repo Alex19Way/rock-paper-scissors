e719b6457167d53832fe091515b2a0c4ef733f07
Alessandro Soldani <alessandro.soldani26@gmail.com>
2021-12-17 17:36:40 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -8,21 +8,21 @@ function battle(s1){
     document.getElementById("userChoise").textContent=s1;
     document.getElementById("computerChoise").textContent=s2;
     switch(s1+" "+s2){
-        case "sasso forbici":
-        case "carta sasso":
-        case "forbici carta":
+        case "rock scissors":
+        case "paper rock":
+        case "scissors paper":
             document.getElementById("battleresult").textContent="You win";
             user++;
             document.getElementById("UserScore").textContent=user;
             break;
-        case "sasso sasso":
-        case "carta carta":
-        case "forbici forbici":
+        case "rock rock":
+        case "paper paper":
+        case "scissors scissors":
             document.getElementById("battleresult").textContent="It's a draw?";
             break;
-        case "sasso carta":
-        case "carta forbici":
-        case "forbici sasso":
+        case "rock paper":
+        case "paper scissors":
+        case "scissors rock":
             document.getElementById("battleresult").textContent=" You lost...";
             computer++;
             document.getElementById("ComputerScore").textContent=computer;
@@ -42,14 +42,34 @@ function random(){
     let a=Math.floor(Math.random() *3)+1;
     switch(a){
         case 1:
-            return "sasso";
+            return "rock";
             break;
         case 2:
-            return "carta";
+            return "paper";
             break;
         case 3:
-            return "forbici";
+            return "scissors";
             break;
         
     }
+}
+
+const f1=document.querySelector("#f1");
+const f2=document.querySelector("#f2");
+const f3=document.querySelector("#f3");
+const f4=document.querySelector("#f4");
+const weapons=document.querySelector(".weapons");
+const pointers=document.querySelector(".pointers");
+const skip=document.querySelector(".skip");
+
+skip.addEventListener("click",jump);
+
+function jump(){
+f1.style.animation="null";
+f2.style.animation="null";
+f3.style.animation="null";
+f4.style.animationDelay="0s";
+weapons.style.animationDelay="0s";
+pointers.style.animationDelay="0s";
+skip.classList.add("dissapear");
 }
